35fe70896da61d05e164643edf58efc5e02d898a
mohammed jamal <yamni.zero@gmail.com>
2022-08-10 12:21:23 +0200

    signup

--- connect.php
+++ connect.php
@@ -12,7 +12,7 @@ try {
    header("Access-Control-Allow-Origin: *");
    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
    header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
-   include "./functions.php";
+   include "functions.php";
    if (!isset($notAuth)) {
       // checkAuthenticate();
    }
