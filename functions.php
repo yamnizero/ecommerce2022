9dfa824a34c986c6fb6dc3c7da4d63f8c8b2f130
mohammed jamal <yamni.zero@gmail.com>
2022-08-17 01:16:36 +0200

    done fun

--- functions.php
+++ functions.php
@@ -156,5 +156,4 @@ function   printFailure($message = "none")
 function sendEmail($to , $title , $body){
 $header = "From: support@waelabohamza.com " . "\n" . "CC: waeleagle1243@gmail.com" ; 
 mail($to , $title , $body , $header) ; 
-echo "Success" ; 
 }
