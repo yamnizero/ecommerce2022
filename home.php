8fc70cf3fabea99e4e44f916910507d487a9ce45
mohammed jamal <yamni.zero@gmail.com>
2022-08-31 19:53:10 +0200

    add categories

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+
+include "connect.php";
+
+$alldata = array();
+
+$alldata['status'] = "success";
+
+
+$categories = getAllData("categories",null,null,false);
+
+$alldata['categories'] = $categories;
+
+
+
+echo json_encode($alldata);
+
+?>
