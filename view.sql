5d82535cf30eacc3a1fd88c132288d3b038c5d83
mohammed jamal <yamni.zero@gmail.com>
2022-12-09 15:11:53 +0200

    dsd

--- view.sql
+++ view.sql
@@ -1,3 +1,8 @@
--- CREATE VIEW items1view AS
--- SELECT items.* , categories.* FROM items
--- INNER JOIN categories on items.items_cat = categories.categories_id
+CREATE OR REPLACE items1view AS
+SELECT items.* , categories.* FROM items
+INNER JOIN categories on items.items_cat = categories.categories_id;
+ 
+CREATE OR REPLACE VIEW myfavorite AS 
+SELECT favorite.* ,items.* ,users.users_id FROM favorite
+INNER JOIN users ON users.users_id = favorite.favorite_usersid
+INNER JOIN items ON items.items_id = favorite.favorite_itemsid
