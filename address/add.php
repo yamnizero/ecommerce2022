61d7f0f246070315cf9fbacb8fe2119826e4bc54
mohammed jamal <yamni.zero@gmail.com>
2023-02-19 18:21:41 +0200

    gh

--- address/add.php
+++ address/add.php
@@ -11,11 +11,11 @@
  $long    = filterRequest("long") ;
 
  $data = array(
-    "address_usersid" =>$usersid ,
-    "address_name" =>$name ,
-    "address_city" =>$city ,
-    "address_street" =>$street ,
-    "address_lat" =>$lat ,
-    "address_long" =>$long ,
+    "address_usersid" => $usersid ,
+    "address_name" => $name ,
+    "address_city" => $city ,
+    "address_street" => $street ,
+    "address_lat" => $lat ,
+    "address_long" => $long ,
  );
   insertData($table, $data);
