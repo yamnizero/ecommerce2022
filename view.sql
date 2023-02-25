477feb7cd9de2db1c62101585cb5d4c44aa524e2
mohammed jamal <yamni.zero@gmail.com>
2023-02-25 15:02:26 +0200

    checkout

--- view.sql
+++ view.sql
@@ -12,4 +12,5 @@ INNER JOIN items ON items.items_id = favorite.favorite_itemsid
 CREATE OR REPLACE VIEW cartview as
 SELECT SUM(items.items_price) as itemsprice ,COUNT(cart.cart_itemsid) as countitems, cart.*, items.* FROM cart
 INNER JOIN items ON items.items_id = cart.cart_itemsid
+WHERE cart_orders = 0
 GROUP BY cart.cart_itemsid, cart.cart_userid;
