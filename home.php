c4408bf1a0bbb752bc1bc8e67e817951582a2a25
mohammed jamal <yamni.zero@gmail.com>
2023-06-20 14:27:56 +0200

    ann

--- home.php
+++ home.php
@@ -7,13 +7,19 @@ $alldata = array();
 $alldata['status'] = "success";
 
 
+$announcement = getAllData("announcement","1 = 1 ",null,false);
+ 
+$alldata['announcement'] = $announcement;
+
+
 $categories = getAllData("categories",null,null,false);
 
 $alldata['categories'] = $categories;
 
-$items = getAllData("items1view","items_discount != 0",null,false);
+$items = getAllData("itemstopselling","1 = 1 ORDER BY countitems DESC ",null,false);
 
-$alldata['items'] = $items;
+$alldata['items'] = $items; 
+
 
 
 echo json_encode($alldata);
